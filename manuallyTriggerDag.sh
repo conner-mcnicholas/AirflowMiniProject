1e6d6f5a5f1766ec78e8bdc160d629564f66a1f4
conner-mcnicholas <connermcnicholas@gmail.com>
2022-06-20 22:44:03 -0700

    spacers

--- manuallyTriggerDag.sh
+++ manuallyTriggerDag.sh
@@ -2,8 +2,8 @@
 export DOCKCONT=$(eval sudo docker ps | grep start | cut -f1 -d' ')
 echo 'Manually triggering dag run'
 sudo docker exec $DOCKCONT airflow dags trigger marketvol
-echo 'Copying files created in docker container data directory to ./data'
+printf '\nCopying files created in docker container data directory to ./data'
 sudo docker cp $DOCKCONT:/tmp/data/ ~/AirflowMiniProject/
 export DATELS=$(eval ls -1 ./data/ | tail -1)
-echo 'Resulting analytic query results for latest run ('${DATELS}'):'
+printf '\n\nResulting analytic query results for latest run ('${DATELS}'):\n\n'
 cat ./data/$DATELS/daily_trade_volumes.txt
