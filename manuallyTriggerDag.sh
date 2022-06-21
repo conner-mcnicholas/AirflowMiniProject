27b4c30d59ccf999c97acc756e5a485bcf3cb450
connermcnicholas <38020254+conner-mcnicholas@users.noreply.github.com>
2022-06-20 22:37:15 -0700

    Update manuallyTriggerDag.sh

--- manuallyTriggerDag.sh
+++ manuallyTriggerDag.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+\#!/bin/sh
 export DOCKCONT=$(eval sudo docker ps | grep start | cut -f1 -d' ')
 echo 'Manually triggering dag run'
 sudo docker exec $DOCKCONT airflow dags trigger marketvol
 echo 'Copying files created in docker container data directory to ./data'
 sudo docker cp $DOCKCONT:/tmp/data/ ~/AirflowMiniProject/
-echo 'Latest results: '
-export DATELS=$(eval ls -lh ./data/ | cut -f9 -d' ' | tail -1)
+export DATELS=$(eval ls -1 ./data/ | tail -1)
+echo 'Resulting analytic query results for latest run ('${DATELS}'):'
 cat ./data/$DATELS/daily_trade_volumes.txt
